7eb8f5ca6ee8e3be1ac318b810a7c2c711efaca9
Adrienne Kerr <adriennek@porch.com>
2015-08-23 02:24:09 -0700

    bootstrapify all the things

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
-    $('a').mouseenter(function() {
-        $(this).fadeTo('fast', .75);
+    $('.about-title').mouseenter(function() {
+        $('.about-body').slideDown('slow');
         });
-    $('a').mouseleave(function() {
-        $(this).fadeTo('fast', 1);
-        });
-    $('.head').mouseenter(function() {
-        $(this).next('.content').slideDown('slow');
-        });
-    $('.head').mouseleave(function() {
-        $(this).next('.content').slideUp('slow');
+    $('.about').mouseleave(function() {
+        $('.about-body').slideUp('slow');
         });
 });
